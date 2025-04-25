4dddac43cedc4fdfd024d29907235d7318b035d8
MrRaib <92303543+MrRaib@users.noreply.github.com>
2025-04-25 18:22:55 +0300

    Add files via upload

---
+++ src/layout/footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return <footer className="page-footer flat-pumpkin accent-1">
+<div className="container">
+  <div className="row">
+    <div className="col l6 s12">
+      <h5 className="white-text"></h5>
+    </div>
+  </div>
+</div>
+<div className="footer-copyright">
+  <div className="container">
+  © {new Date().getFullYear()} Разработчик: Чупуков Степан ТКБО-03-23
+  </div>
+</div>
+</footer>
+}
+export { Footer };
